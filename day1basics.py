72c1811d8a0e3539193c3d15a760ddb177568474
EvgeniiPain <evgen0700@icloud.com>
2025-04-07 12:16:53 -0500

    Added updated day1_basics

---
+++ day1basics.py
@@ -0,0 +1,11 @@
+#print("Hello,World!")
+#user_name = input("what is your name?")
+#print ("Hello! Nice to meet you,", user_name)
+#age = int(input("How old are you?"))
+#if age>= 20:
+#    print("you are getting old!")
+#else:
+#    print (" you are a baby.")
+name = input("What is your name?")
+age = input("How old are you?")
+print("Hello,", name + "!", "You are", age, "years old.")
